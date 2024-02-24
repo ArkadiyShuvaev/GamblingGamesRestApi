2c5d97e142a7e892afdf12bd0d73e070d71170eb
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2024-02-24 18:29:19 +0100

    adjusted XML documentation

--- Models/RegisterRequestModel.cs
+++ Models/RegisterRequestModel.cs
@@ -4,11 +4,18 @@ namespace GamblingGamesRestApi.Models;
 
 public class RegisterRequestModel
 {
+    /// <summary>
+    /// Gets or sets the email of the user.
+    /// </summary>
     [Required]
     [EmailAddress]
     [MaxLength(255)]
     public string Email { get; set; }
 
+    /// <summary>
+    /// Gets or sets the password of the user.
+    /// </summary>
+    /// <example>P@ssw0rd!</example>
     [Required]
     public string Password { get; set; }
 }
